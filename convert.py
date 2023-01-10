513bd561c3cf3bf74a7d4935cdf35c0e42feaf62
Canyon Smith <canyons@HBLL-MLAB-05-N.local>
2023-01-10 15:06:30 -0700

    convert file optimized

--- convert.py
+++ convert.py
@@ -1,58 +1,43 @@
 import json
 import re
-
-from nltk import pos_tag
-from nltk.corpus import stopwords, wordnet
+from nltk import pos_tag, wordnet
+from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
-
-def clean_text(text):
-    text = " ".join(text.split())
-    return re.sub("[^A-Za-z0-9 ]+", "", text)
-
-
-def read_text_file(filename):
-    with open(filename, "r") as f:
-        text = f.read()
-    return text
-
-
-def lemmatize_text(text):
+def preprocess_text(text):
+    """Preprocesses a string by removing stop words, stemming, and lemmatizing."""
+    stop_words = set(stopwords.words("english"))
+    word_tokens = word_tokenize(text)
+    text = " ".join([word for word in word_tokens if word not in stop_words])
+    stemmer = PorterStemmer()
+    text = " ".join([stemmer.stem(word) for word in text.split()])
     lemmatizer = WordNetLemmatizer()
     return " ".join([lemmatizer.lemmatize(word) for word in text.split()])
 
-
-# remove stop words
-def remove_stop_words(text):
-    stop_words = set(stopwords.words("english"))
-    word_tokens = word_tokenize(text)
-    return " ".join([word for word in word_tokens if word not in stop_words])
-
-
-def stem_text(text):
-    stemmer = PorterStemmer()
-    return " ".join([stemmer.stem(word) for word in text.split()])
-
-
 def create_dict_list(text):
+    """Creates a list of dictionaries from a string."""
     text_list = text.split("\n")
     return [{"text": item} for item in text_list if item != ""]
 
-
 def convert_to_json(filename):
-    text = read_text_file(filename)
-    text = clean_text(text)
-    text = remove_stop_words(text)
-    text = stem_text(text)
-    text = lemmatize_text(text)
+    """Converts a text file to a JSON string."""
+    with open(filename, "r") as file:
+        text = file.read()
+    text = re.sub("[^A-Za-z0-9 ]+", "", text)
+    text = preprocess_text(text)
     text_list = create_dict_list(text)
-    json_text = json.dumps(text_list)
-    # print(json_text)
+    print(text_list)
+    tagged_text = pos_tag(text_list[0]["text"].split())
+    json_text = json.dump(text_list, open("output.json", "w"))
     for item in text_list:
-        print(item["text"], end=" ")
+        if not isinstance(item, dict):
+            print(item["text"], end=" ")
     return json_text
 
-
 if __name__ == "__main__":
+    import time
+    start = time.time()
     convert_to_json(input("Enter file name: "))
+    end = time.time()
+    print(f"Time taken: {end - start} seconds")
